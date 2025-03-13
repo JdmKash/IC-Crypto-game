28b4305c06bd1758541abfbbb57ff5d71182c13a
Jannym0ney <jannym0ney@2603-9001-8900-16e8-0000-0000-0000-114d.lan>
2025-03-13 15:55:11 -0400

    Update next.config.js to use src directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'dist',
+  experimental: {
+    appDir: true,
+  },
+  // Specify the source directory
+  srcDir: 'src',
 };
 
 module.exports = nextConfig;
